7435bb65cf234eddc30a4f815073eb11b47dc0cc
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-17 23:02:06 +0300

    add Task001 Sem 3

---
+++ Task001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и 
+// выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите натуральное число");
+int A = Convert.ToInt32(Console.ReadLine());
+Summa(A);
+Console.WriteLine($"Сумма по 2 функции всех чисел от 1 до {A} равна {Summa2(A)}");
+
+
+
+
+void Summa(int A)
+{
+    int sum = 0;
+    for (int i=1; i<=A; i++)
+        sum+=i;
+    Console.WriteLine($"Сумма всех чисел от 1 до {A} равна {sum}");
+}
+
+int Summa2(int A)
+{
+    int sum = 0;
+    for (int i=1; i<=A; i++)
+        sum+=i;
+    return sum;
+}
